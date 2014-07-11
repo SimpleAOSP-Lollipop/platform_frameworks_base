ec317f9d0a69ef103b727354031c115bc4854dee
Roman Birg <roman@cyngn.com>
2014-07-11 09:14:50 -0700

    SystemUI: fix double tap to sleep not working with secure lock screen
    Double tap to sleep on status bar would only work on secure lockscreens if the
    slider brightness control was enabled.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2593,9 +2593,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (mBrightnessControl) {
             brightnessControl(event);
-            if ((mDisabled & StatusBarManager.DISABLE_EXPAND) != 0) {
-                return true;
-            }
+        }
+        if ((mDisabled & StatusBarManager.DISABLE_EXPAND) != 0) {
+            return true;
         }
 
         final boolean upOrCancel =
