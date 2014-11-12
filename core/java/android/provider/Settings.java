687b196cfaf325fd00186c8bbb16ccb2e38fdd39
rascarlo <rascarlo@gmail.com>
2014-11-12 16:12:20 +0100

    kill app long press back

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4855,6 +4855,12 @@ public final class Settings {
         public static final String SLEEP_TIMEOUT = "sleep_timeout";
 
         /**
+          * Whether to allow killing of the foreground app by long-pressing the Back button
+          * @hide
+          */
+         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
