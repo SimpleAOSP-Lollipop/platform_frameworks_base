58da6e1df68a1a57310ca005fd1f5e8c6a5be4ef
cmorlok <ChristianMorlok@gmail.com>
2014-11-25 13:31:56 +0000

    [1/2] Base: Hide power menu on lockscreen
    Adds an option to enable/disable power menu on lockscreen.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3058,6 +3058,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
+         * This preference enables showing the power menu on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
