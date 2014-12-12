e8a324cbcf6399ae472ebe34bdadb14213d57766
Roman Birg <roman@cyngn.com>
2014-12-11 16:45:54 -0800

    framework: add blacklist disconnect cause
    Constant to be used when marking a Call as blacklisted.
    
    Change-Id: If8e0ba7a2fb9b3ebbcfde04df48cd0ab14bba864
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- telephony/java/android/telephony/DisconnectCause.java
+++ telephony/java/android/telephony/DisconnectCause.java
@@ -160,6 +160,12 @@ public class DisconnectCause {
      */
     public static final int OUTGOING_CANCELED = 44;
 
+    /**
+     * Call was rejected due to number being blacklisted by user.
+     * {@@hide}
+     */
+    public static final int CALL_BLACKLISTED = 400;
+
     /** Smallest valid value for call disconnect codes. */
     public static final int MINIMUM_VALID_VALUE = NOT_DISCONNECTED;
 
@@ -262,6 +268,8 @@ public class DisconnectCause {
             return "OUTGOING_FAILURE";
         case OUTGOING_CANCELED:
             return "OUTGOING_CANCELED";
+        case CALL_BLACKLISTED:
+            return "CALL_BLACKLISTED";
         default:
             return "INVALID: " + cause;
         }
