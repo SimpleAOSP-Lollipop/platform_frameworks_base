233d1e42f333853abebf70fb6afa7320f27ca468
snak3ater <saeedyasir91@gmail.com>
2014-12-15 14:04:48 +0500

    Revert "Framework: StatusBar Ticker (1/2)"
    This reverts commit f7225840cff747d5a06befc80efc2856f3877f51.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2107,13 +2107,6 @@ public final class Settings {
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
         /**
-         * Whether to enable ticker animation in statusbar
-         *
-         * @hide
-         */
-        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
-
-        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
