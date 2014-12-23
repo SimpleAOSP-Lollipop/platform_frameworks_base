2a20fd56bfe175cd8d03421c54742296508fa0e5
dankoman <dankoman30@gmail.com>
2014-12-23 17:57:21 +0100

    [1/2] Frameworks: Doze (ambient display) timeout
    Change-Id: I0eb230020da9a306e089b58db9d48b18c9743c0f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3294,6 +3294,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFCATION_DECAY = "heads_up_notification_decay";
 
         /**
+         * Timeout for ambient display notification
+         * @hide
+         */
+        public static final String DOZE_TIMEOUT = "doze_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
