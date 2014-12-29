426037d8fc5fdab7b585d16ba02a2e8b57b60aa8
Brian Larsen <brian@chezbel.com>
2014-12-29 14:29:57 +0800

    Bootmsg: The app name is now displayed in bold text.
    Change-Id: I5e37527e3b00ef3889a029c707b54a514c7f0a22

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -70,6 +70,7 @@ import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
+import android.text.Html;
 import android.service.gesture.EdgeGestureManager;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
@@ -5690,7 +5691,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (always && (currentPackageName != null)) {
                     // Only display the current package name if the main message says "Optimizing app N of M".
                     // We don't want to do this when the message says "Starting apps" or "Finishing boot", etc.
-                    mBootMsgDialog.setMessage(msg + "\n" + currentPackageName);
+//                    mBootMsgDialog.setMessage(msg + "\n" + currentPackageName);
+                    mBootMsgDialog.setMessage(Html.fromHtml(msg + "<br><b>" + currentPackageName + "</b>"));
                 }
                 else {
                     mBootMsgDialog.setMessage(msg);
