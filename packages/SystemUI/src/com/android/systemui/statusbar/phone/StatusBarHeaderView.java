bebb2875bc4ae957ba8d7c7cc8986e1dc7679cb6
cristianomatos <cristianobmatos@gmail.com>
2015-01-09 18:16:10 -0200

    Correct fix "ghost" weather display in expanded status bar
    - Thanks to the hint of @Altaf-Mahdi. All credits gos to him. check our comments here: https://github.com/crdroidandroid/android_frameworks_base/commit/607175f0fe6469600e01339f2b93e4b1edabc275

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -833,7 +833,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             final boolean showingDetail = detail != null;
             transition(mClock, !showingDetail);
             transition(mDateGroup, !showingDetail);
-            transition(mWeatherContainer, !showingDetail);
+            if (mShowWeather) {
+                transition(mWeatherContainer, !showingDetail);
+            }
             if (mAlarmShowing) {
                 transition(mAlarmStatus, !showingDetail);
             }
@@ -859,7 +861,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             } else {
                 mQsDetailHeader.setClickable(false);
             }
-            mWeatherContainer.setVisibility(mExpanded && mShowWeather ? View.VISIBLE : View.GONE);
         }
 
         private void transition(final View v, final boolean in) {
