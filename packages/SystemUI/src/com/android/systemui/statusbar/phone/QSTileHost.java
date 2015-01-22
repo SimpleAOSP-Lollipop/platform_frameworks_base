90e6c7a1e3df82d3f69d74113a9bd1be42f7b785
Adnan Begovic <adnan@cyngn.com>
2015-01-21 18:54:39 -0800

    SystemUI: Create LteTile
    Change-Id: Ic193279f930af54d5754358c7e4c97b6b07d2010

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
+import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
@@ -290,6 +291,8 @@ public class QSTileHost implements QSTile.Host {
                 return new NfcTile(this);
             case QSConstants.TILE_LOCKSCREEN:
                 return new LockscreenToggleTile(this);
+            case QSConstants.TILE_LTE:
+                return new LteTile(this);
 	    case QSConstants.TILE_SCREEN_TIMEOUT:
 		return new ScreenTimeoutTile(this);
             default:
