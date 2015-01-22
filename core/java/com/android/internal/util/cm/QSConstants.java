a37b3e9b18b21d7c4dbf21e132face35acd1dd0c
Adnan Begovic <adnan@cyngn.com>
2015-01-21 18:54:39 -0800

    SystemUI: Create LteTile
    Change-Id: Ic193279f930af54d5754358c7e4c97b6b07d2010

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -39,6 +39,7 @@ public class QSConstants {
     public static final String TILE_NFC = "nfc";
     public static final String TILE_COMPASS = "compass";
     public static final String TILE_LOCKSCREEN = "lockscreen";
+    public static final String TILE_LTE = "lte";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
 
     // Order matters
@@ -69,6 +70,7 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_COMPASS);
         TILES_AVAILABLE.add(TILE_LOCKSCREEN);
+        TILES_AVAILABLE.add(TILE_LTE);
 	TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
     }
 }
