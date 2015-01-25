2603fcda08825dc74edbc2ecb3d49c4806d2a5cb
Allan Hedelain <tristan202@gmail.com>
2015-01-25 18:37:01 +0000

    SystemUI: add back status bar date options (1/2)
    * Date style
    * Date format
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I03f70aa9f0c632727b4f1465842bb75839d0de46

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3075,6 +3075,21 @@ public final class Settings {
          */
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
+        /**
+         * Shows custom date before clock time
+         * 0 - No Date
+         * 1 - Small Date
+         * 2 - Normal Date
+         * @hide
+         */
+        public static final String STATUS_BAR_DATE = "status_bar_date";
+
+        /**
+         * Stores the java DateFormat string for the date
+         * @hide
+         */
+        public static final String STATUS_BAR_DATE_FORMAT = "status_bar_date_format";
+
 	/**
          * Boolean value on whether to show weather in the statusbar
 	 * @hide
