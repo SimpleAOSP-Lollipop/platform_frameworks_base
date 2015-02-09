3231060b16ac45403aeb25d64ae733013e504e4d
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:48:03 +0000

    QS: add Screen off tile
    * Long press for power menu
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ib40968ed38edeb26fa657c69106d446c6ebdb39e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -49,6 +49,7 @@ import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.HeadsupTile;
+import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -299,6 +300,8 @@ public class QSTileHost implements QSTile.Host {
                 return new BrightnessTile(this);
             case QSConstants.TILE_BATTERY_SAVER:
                 return new BatterySaverTile(this);
+            case QSConstants.TILE_SCREEN_OFF:
+                return new ScreenOffTile(this);
 	    case QSConstants.TILE_HEADS_UP:
 		return new HeadsupTile(this);
             default:
