ec15e0932093ecfd2789b9151cbfc7717d07090c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:32:00 +0000

    QS: add Brightness tile
    * long press to toggle auto/manual brightness
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: I173281dbaba5945f0f99071639ddccee6a6ca365

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -33,6 +33,7 @@ import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.BrightnessTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
@@ -293,6 +294,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenshotTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
+            case QSConstants.TILE_BRIGHTNESS:
+                return new BrightnessTile(this);
 	    case QSConstants.TILE_HEADS_UP:
 		return new HeadsupTile(this);
             default:
