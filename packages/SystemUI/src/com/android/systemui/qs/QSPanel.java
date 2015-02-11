9b83813c61aa4dd5b2b6272eddcf31aaad812f18
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-11 17:42:04 +0000

    Revert "QS: fix random missing tiles when leaving detailed view"
    This reverts commit 47d342bd6a02a31b492033028618389f14d3493c.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -622,7 +622,7 @@ public class QSPanel extends ViewGroup {
 
         @Override
         public void onAnimationEnd(Animator animation) {
-            setGridContentVisibility(true);
+            setGridContentVisibility(false);
         }
     };
 
