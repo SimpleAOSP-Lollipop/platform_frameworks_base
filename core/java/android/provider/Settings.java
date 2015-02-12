1b3e2f2bebf84ff69eb9f2cefda94146d80fed08
Lars Greiss <kufikugel@googlemail.com>
2015-02-12 21:12:10 +0500

    IME feature: Force to show always enter key [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3321,6 +3321,12 @@ public final class Settings {
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
                 "mute_annoying_notifications_threshold";
 
+	/**
+         * Forces formal text input.  1 to replace emoticon key with enter key.
+         * @hide
+         */
+        public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
